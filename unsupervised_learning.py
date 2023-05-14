d500ffe694f379e51bb729d2d2a4d3080a466c97
NikhilSharma1234 <nikhilsharma@nevada.unr.edu>
2023-05-14 09:25:19 +0000

    unsupervised learning functions

--- unsupervised_learning.py
+++ unsupervised_learning.py
@@ -3,14 +3,31 @@
 # CS 345 - Cyber AI
 # unsupervised_learning.py
 
+from sklearn.cluster import KMeans
+import numpy as np
+
 def unsup_binary_train(X_train, y_train):
-    print("unsup_binary_train")
+    return KMeans(n_clusters=2).fit(X_train, y_train)
     
 def unsup_binary_test(model, X_test, y_test):
-    print("unsup_binary_test")
+    correct = 0
+    total = 0
+    predictions = model.predict(X_test)
+    for i in range(len(predictions)):
+        total += 1
+        if predictions[i] == y_test.iloc[i]:
+            correct += 1
+    return(correct/total)
 
 def unsup_multiclass_train(X_train, y_train, k):
-    print("unsup_multiclass_train")
+    return KMeans(n_clusters=k).fit(X_train, y_train)
 
 def unsup_multiclass_test(model, X_test, y_test):
-    print("unsup_multiclass_test")
+    correct = 0
+    total = 0
+    predictions = model.predict(X_test)
+    for i in range(len(predictions)):
+        total += 1
+        if predictions[i] == y_test.iloc[i]:
+            correct += 1
+    return(correct/total)
